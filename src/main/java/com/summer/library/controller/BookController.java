037007d3b29666a00a9e3fd4e92dc69727d06098
Abhisek Bagdas <abhisekbagdas@gmail.com>
2025-07-21 12:51:54 +0545

    postman initialized

--- src/main/java/com/summer/library/controller/BookController.java
+++ src/main/java/com/summer/library/controller/BookController.java
@@ -2,19 +2,26 @@ package com.summer.library.controller;
 
 import com.summer.library.dao.BookDAO;
 import com.summer.library.models.Book;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
+@RestController
+@RequestMapping("/api")
 public class BookController {
 
-    public void storeBook(Book book){
-        BookDAO bookdao = new BookDAO();
-        bookdao.insertBook(book);
-    }
 
+    @GetMapping("/books")
     public ArrayList<Book> getBooks() {
         BookDAO bookdao = new BookDAO();
         return bookdao.getBooks();
+    }
+
+
+    @PostMapping("/books")
+    public void storeBook(@RequestBody Book book){
+        BookDAO bookdao = new BookDAO( );
+        bookdao.insertBook(book);
     }
 
     public void updateBook(int bookid, int bookNumber) {
